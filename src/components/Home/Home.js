b53d2a25e2793ffde4beb74905561d03024883e7
mahfuz-antor <antorcnj2014@gmail.com>
2021-06-10 17:46:21 +0600

    netlify error handle

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
                     </div>
                     <div class="p-6">
                         <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold"></div>
-                        <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">The beautiful place Sylhet of Bangladesh</a>
+                        <a href="https://mahfuz-antor.web.app/" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">The beautiful place Sylhet of Bangladesh</a>
                         <p class="mt-2 text-gray-500">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia aut beatae ducimus provident, vero nam enim, ipsa maxime deleniti mollitia pariatur ex earum totam perferendis itaque quidem architecto repellat suscipit dignissimos cupiditate delectus! Ex ad nam, nobis tenetur aut facilis..</p>
                     </div>
                 </div>
@@ -28,7 +28,7 @@ const Home = () => {
                     </div>
                     <div class="p-6">
                         <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold"></div>
-                        <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">The beautiful place Sylhet of Bangladesh</a>
+                        <a href="https://mahfuz-antor.web.app/" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">The beautiful place Sylhet of Bangladesh</a>
                         <p class="mt-2 text-gray-500">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia aut beatae ducimus provident, vero nam enim, ipsa maxime deleniti mollitia pariatur ex earum totam perferendis itaque quidem architecto repellat suscipit dignissimos cupiditate delectus! Ex ad nam, nobis tenetur aut facilis.</p>
                     </div>
                 </div>
@@ -42,7 +42,7 @@ const Home = () => {
                     </div>
                     <div class="p-6">
                         <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold"></div>
-                        <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"> The beautiful place Rangamaty of Bangladesh</a>
+                        <a href="https://mahfuz-antor.web.app/" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"> The beautiful place Rangamaty of Bangladesh</a>
                         <p class="mt-2 text-gray-500">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia aut beatae ducimus provident, vero nam enim, ipsa maxime deleniti mollitia pariatur ex earum totam perferendis itaque quidem architecto repellat suscipit dignissimos cupiditate delectus! Ex ad nam, nobis tenetur aut facilis.</p>
                     </div>
                 </div>
